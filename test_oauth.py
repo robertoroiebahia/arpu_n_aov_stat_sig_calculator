90716f8fee2c50bc81d84c4e6156193ada7f2795
robertoroiebahia <60297138+robertoroiebahia@users.noreply.github.com>
2025-02-17 18:32:27 -0300

    Update test_oauth.py

--- test_oauth.py
+++ test_oauth.py
@@ -1,22 +1,26 @@
 import os
-
+import streamlit as st
 import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
-import streamlit as st
 import webbrowser
 
-
+# Use the redirect URI from an environment variable or default to localhost:8501
 redirect_uri = os.environ.get("REDIRECT_URI", "http://localhost:8501/")
-
 
 def auth_flow():
     st.write("Welcome to My App!")
     auth_code = st.query_params.get("code")
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        "client_secret.json", # replace with you json credentials from your google auth app
+    
+    # Load client configuration from Streamlit Secrets (TOML format)
+    client_config = st.secrets["GOOGLE_CLIENT_SECRETS"]
+    
+    # Create the OAuth flow using the client configuration.
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(
+        client_config,
         scopes=["https://www.googleapis.com/auth/userinfo.email", "openid"],
         redirect_uri=redirect_uri,
     )
+    
     if auth_code:
         flow.fetch_token(code=auth_code)
         credentials = flow.credentials
@@ -38,16 +42,13 @@ def auth_flow():
             )
             webbrowser.open_new_tab(authorization_url)
 
-
 def main():
     if "google_auth_code" not in st.session_state:
         auth_flow()
-
+    
     if "google_auth_code" in st.session_state:
         email = st.session_state["user_info"].get("email")
         st.write(f"Hello {email}")
 
-
 if __name__ == "__main__":
     main()
-
